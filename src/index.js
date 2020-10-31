668993be3ea349f79b736a64e84d78fff6cee771
Ivan <ivantbv@gmail.com>
2020-11-01 00:18:50 +0500

    added a tabs library to the project, background image and some concepts. Might re-think the need for this library and do it the vanilla way

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('WORKS YAY!!!')
+import { pageLoad } from './pageLoad'
+import jTabs from 'jtabs';
+pageLoad();
+
+
+var container = document.querySelector('.tabs-container');
+ 
+jTabs({
+    container      : container,
+    buttons        : container.querySelectorAll('.btn-js'),
+    tabs           : container.querySelectorAll('.tab-js'),
+    activeBtnClass : 'btn-active-js',
+    activeTabClass : 'tab-active-js'
+});
---
+++ src/pageLoad.js
@@ -0,0 +1,15 @@
+const pageLoad = () => {
+    const content = document.querySelector('#content');
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Restaurant\'s title';
+    const h3 = document.createElement('h3');
+    h3.textContent = 'Welcome To The Restaurant';
+    // const p = document.createElement('p');
+    // p.textContent = 'Lorem ipsum dolor sit amet dummy texto'
+    content.appendChild(h1)
+    content.appendChild(h3);
+
+}
+
+export { pageLoad };
+
