ac2425bc05ada1e1d16f500d80361a04b966a938
Ivan <ivantbv@gmail.com>
2020-11-01 15:25:29 +0500

    dropped the jtabs library and decided to do the tabs the vanilla way. Next up is to apply the logic in javascript how each tab is going to display their own content

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import { pageLoad } from './pageLoad'
-import jTabs from 'jtabs';
+// import jTabs from 'jtabs';
 pageLoad();
 
 
-var container = document.querySelector('.tabs-container');
+// let container = document.querySelector('.tabs-container');
  
-jTabs({
-    container      : container,
-    buttons        : container.querySelectorAll('.btn-js'),
-    tabs           : container.querySelectorAll('.tab-js'),
-    activeBtnClass : 'btn-active-js',
-    activeTabClass : 'tab-active-js'
-});
+// jTabs({
+//     container      : container,
+//     buttons        : container.querySelectorAll('.btn-js'),
+//     tabs           : container.querySelectorAll('.tab-js'),
+//     activeBtnClass : 'btn-active-js',
+//     activeTabClass : 'tab-active-js'
+// });
